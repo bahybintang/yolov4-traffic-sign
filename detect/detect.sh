8e13583a525eb0019f52675a1d17f8972fdf7a89
Muhammad Bintang Bahy <bahybintang@gmail.com>
2021-08-10 15:08:20 +0700

    Update detection

--- detect/detect.sh
+++ detect/detect.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+find images -name "*.jpg" >images.txt
 ./darknet detector test obj.data yolov4-obj.cfg yolov4-obj_best.weights -dont_show -out result.json <images.txt
 python3 detect.py
